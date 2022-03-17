0706baca0075e5269ce70d5b98c1f5a7630520b8
Just-JC <65417594+Just-JC@users.noreply.github.com>
2022-03-17 21:30:26 +0100

    add index for new files

---
+++ snakecore/config.py
@@ -0,0 +1,31 @@
+"""
+This file is a part of the source code for the PygameCommunityBot.
+This project has been licensed under the MIT license.
+Copyright (c) 2020-present PygameCommunityDiscord
+
+This file defines some constants and variables used across the whole codebase
+"""
+
+import io
+import os
+from typing import Optional, Union
+
+import discord
+
+client: Optional[discord.Client] = None
+
+# 'primary' guild for the bot
+guild: Optional[discord.Guild] = None
+
+# IO object to redirect output to discord, gets patched later
+stdout: Optional[io.StringIO] = None
+
+console_channel: discord.TextChannel
+db_channel: discord.TextChannel
+
+BASIC_MAX_FILE_SIZE = 8_000_000  # bytes
+
+ZERO_SPACE = "\u200b"  # U+200B
+
+DOC_EMBED_LIMIT = 3
+BROWSE_MESSAGE_LIMIT = 500
