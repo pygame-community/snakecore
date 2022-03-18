2778ed579de7fe49d8db18789364680fc22350f2
Ankith <46915066+ankith26@users.noreply.github.com>
2022-03-18 18:51:06 +0530

    minor setup.py changes + black

--- setup.py
+++ setup.py
@@ -1,53 +1,54 @@
-from setuptools import setup
 import re
 
-requirements = []
-with open('requirements.txt') as f:
-  requirements = f.read().splitlines()
+from setuptools import setup
 
-version = ''
-with open('snakecore/__init__.py') as f:
-    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
 
-if not version:
-    raise RuntimeError('version is not set')
+with open("snakecore/__init__.py") as f:
+    searched = re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE
+    )
+    version = searched.group(1) if searched is not None else ""
+    if not version:
+        raise RuntimeError("version is not set")
 
-readme = ''
-with open('README.md') as f:
+with open("README.md") as f:
     readme = f.read()
 
-packages = [
-    'snakecore',
-]
-
-setup(name='snakecore',
-      author='PygameCommunityDiscord',
-      url='https://github.com/PygameCommunityDiscord/snakecore',
-      project_urls={
+packages = ["snakecore"]
+setup(
+    name="snakecore",
+    author="PygameCommunityDiscord",
+    url="https://github.com/PygameCommunityDiscord/snakecore",
+    project_urls={
         "Issue tracker": "https://github.com/PygameCommunityDiscord/snakecore/issues",
-      },
-      version=version,
-      packages=packages,
-      license='MIT',
-      description='A set of core APIs to facilitate the creation of feature-rich Discord bots.',
-      long_description=readme,
-      long_description_content_type="text/markdown",
-      include_package_data=True,
-      install_requires=requirements,
-      python_requires='>=3.9.0',
-      classifiers=[
+    },
+    version=version,
+    packages=packages,
+    license="MIT",
+    description=(
+        "A set of core APIs to facilitate the creation of feature-rich Discord bots."
+    ),
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    install_requires=requirements,
+    python_requires=">=3.9.0",
+    platforms=["any"],
+    classifiers=[
         "Topic :: Software Development",
-        'License :: OSI Approved :: MIT License',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Development Status :: 2 - Pre-Alpha',
-        'Topic :: Internet',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Utilities',
-        'Typing :: Typed',
-      ]
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Development Status :: 2 - Pre-Alpha",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+        "Typing :: Typed",
+    ],
 )
--- snakecore/__init__.py
+++ snakecore/__init__.py
@@ -1,7 +1,7 @@
-__title__ = 'snakecore'
-__author__ = 'PygameCommunityDiscord'
-__license__ = 'MIT'
-__copyright__ = 'Copyright 2022-present PygameCommunityDiscord'
-__version__ = '0.1.0'
-
 from . import command_handler, db, events, jobs, utils
+
+__title__ = "snakecore"
+__author__ = "PygameCommunityDiscord"
+__license__ = "MIT"
+__copyright__ = "Copyright 2022-present PygameCommunityDiscord"
+__version__ = "0.1.0"
