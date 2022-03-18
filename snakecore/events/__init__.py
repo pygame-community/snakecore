44cb1d66d1db16048b6aa21b8f085edbc3816773
Just-JC <65417594+Just-JC@users.noreply.github.com>
2022-03-18 18:59:32 +0100

    remove some code from 'events' and 'utils' modules 2

--- snakecore/events/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-This file is a part of the source code for snakecore.
-This project has been licensed under the MIT license.
-Copyright (c) 2020-present PygameCommunityDiscord
-
-This module implements classes for representing generic event objects using OOP principles.
-"""
