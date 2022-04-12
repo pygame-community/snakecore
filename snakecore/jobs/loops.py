5bee80d639d4f07d889d5ce6ef3266cd33a01e9c
Mega-JC <65417594+Mega-JC@users.noreply.github.com>
2022-04-12 15:22:26 +0200

    add MiniJobBase for non-managed job objects, several improvements and bug fixes

---
+++ snakecore/jobs/loops.py
@@ -0,0 +1,136 @@
+"""This file is a part of the source code for snakecore.
+This project has been licensed under the MIT license.
+Copyright (c) 2022-present PygameCommunityDiscord
+
+This file defines specialized subclasses of `discord.ext.tasks.Loop`,
+which are used by job objects.
+"""
+
+import asyncio
+import datetime
+from typing import Any
+import discord
+
+from discord.ext.tasks import _log, ExponentialBackoff, Loop, MISSING
+
+from snakecore.constants import DEFAULT_JOB_EXCEPTION_WHITELIST
+
+
+class CustomLoop(Loop):
+    """A small subclass of `discord.ext.tasks.LoTaop`
+    for getting more control over the Task cancelling process among other
+    things.
+    """
+
+    def __init__(self, coro, seconds, hours, minutes, time, count, reconnect):
+        super().__init__(coro, seconds, hours, minutes, time, count, reconnect)
+        self.clear_exception_types()
+        self.add_exception_type(*DEFAULT_JOB_EXCEPTION_WHITELIST)
+
+    def cancel(self):
+        """Cancels the internal task, if it is running."""
+        if self._can_be_cancelled():
+            self._task.cancel(msg="CANCEL_BY_TASK_LOOP")
+
+    def restart(self, *args, **kwargs):
+        r"""A convenience method to restart the internal task.
+
+        .. note::
+
+            Due to the way this function works, the task is not
+            returned like :meth:`start`.
+
+        Parameters
+        ------------
+        \*args
+            The arguments to to use.
+        \*\*kwargs
+            The keyword arguments to use.
+        """
+
+        def restart_when_over(fut, *, args=args, kwargs=kwargs):
+            self._task.remove_done_callback(restart_when_over)
+            self.start(*args, **kwargs)
+
+        if self._can_be_cancelled():
+            self._task.add_done_callback(restart_when_over)
+            self._task.cancel(msg="CANCEL_BY_TASK_LOOP")
+
+    async def _loop(self, *args: Any, **kwargs: Any) -> None:
+        backoff = None
+        await self._call_loop_function("before_loop")
+        self._last_iteration_failed = False
+        if self._time is not MISSING:
+            self._next_iteration = self._get_next_sleep_time()
+        else:
+            self._next_iteration = datetime.datetime.now(datetime.timezone.utc)
+            await asyncio.sleep(0)  # allows canceling in before_loop
+        try:
+            if self._stop_next_iteration:  # allow calling stop() before first iteration
+                return
+            while True:
+                # sleep before the body of the task for explicit time intervals
+                if self._time is not MISSING:
+                    await self._try_sleep_until(self._next_iteration)
+                if not self._last_iteration_failed:
+                    self._last_iteration = self._next_iteration
+                    self._next_iteration = self._get_next_sleep_time()
+
+                    # In order to account for clock drift, we need to ensure that
+                    # the next iteration always follows the last iteration.
+                    # Sometimes asyncio is cheeky and wakes up a few microseconds before our target
+                    # time, causing it to repeat a run.
+                    while (
+                        self._time is not MISSING
+                        and self._next_iteration <= self._last_iteration
+                    ):
+                        _log.warn(
+                            (
+                                "Clock drift detected for task %s. Woke up at %s but needed to sleep until %s. "
+                                "Sleeping until %s again to correct clock"
+                            ),
+                            self.coro.__qualname__,
+                            discord.utils.utcnow(),
+                            self._next_iteration,
+                            self._next_iteration,
+                        )
+                        await self._try_sleep_until(self._next_iteration)
+                        self._next_iteration = self._get_next_sleep_time()
+
+                try:
+                    await self.coro(*args, **kwargs)
+                    self._last_iteration_failed = False
+                except self._valid_exception:
+                    self._last_iteration_failed = True
+                    if not self.reconnect:
+                        raise
+                    elif backoff is None:
+                        backoff = ExponentialBackoff()
+                    await asyncio.sleep(backoff.delay())
+                else:
+                    if self._stop_next_iteration:
+                        return
+
+                    # sleep after the body of the task for relative time intervals
+                    if self._time is MISSING:
+                        await self._try_sleep_until(self._next_iteration)
+
+                    self._current_loop += 1
+                    if self._current_loop == self.count:
+                        break
+
+        except asyncio.CancelledError:
+            self._is_being_cancelled = True
+            raise
+        except Exception as exc:
+            self._has_failed = True
+            await self._call_loop_function("error", exc)
+            raise exc
+        finally:
+            await self._call_loop_function("after_loop")
+            if self._handle:
+                self._handle.cancel()
+            self._is_being_cancelled = False
+            self._current_loop = 0
+            self._stop_next_iteration = False
+            self._has_failed = False
---
+++ snakecore/jobs/minijobs.py
@@ -0,0 +1,265 @@
+"""This file is a part of the source code for snakecore.
+This project has been licensed under the MIT license.
+Copyright (c) 2022-present PygameCommunityDiscord
+
+This file implements the base classes for mini job objects,
+which are stripped-down versions of the regular types of job objects.
+"""
+
+import datetime
+import time
+from typing import Optional, Sequence, Union
+
+import discord
+from snakecore.constants import UNSET, _UnsetType
+from snakecore.exceptions import JobStateError
+
+from snakecore.jobs.jobs import _JobBase
+from snakecore.jobs.loops import CustomLoop
+
+NoneType = type(None)
+
+
+class MiniJobBase(_JobBase):
+    """Base class for interval based mini jobs.
+
+    A mini job is a stripped-down version of a job that doesn't run in a job manager.
+    This class supports only some of the API of `JobBase` subclasses, which are
+    initialization, starting, running, stopping and restarting. However, it is not a
+    subclass of `JobBase`.
+
+    Mini jobs can run on their own and can be used in cases where the advanced features
+    a job manager provides are not needed, or where a job wants to manage the execution
+    of jobs on its own.
+
+    Subclasses are expected to overload the `on_run()` method.
+    Other methods prefixed with `on_` can optionally be overloaded.
+
+    One can override the class variables `DEFAULT_INTERVAL`,
+    `DEFAULT_COUNT` and `DEFAULT_RECONNECT` in subclasses.
+    They are derived from the keyword arguments of the
+    `discord.ext.tasks.Loop` constructor. These will act as
+    defaults for each job object created from this class.
+    """
+
+    DEFAULT_INTERVAL = datetime.timedelta()
+    DEFAULT_TIME: Optional[Union[datetime.time, Sequence[datetime.time]]] = None
+
+    DEFAULT_COUNT: Optional[int] = None
+    DEFAULT_RECONNECT = True
+
+    def __init__(
+        self,
+        interval: Union[datetime.timedelta, _UnsetType] = UNSET,
+        time: Union[datetime.time, Sequence[datetime.time], _UnsetType] = UNSET,
+        count: Union[int, NoneType, _UnsetType] = UNSET,
+        reconnect: Union[bool, _UnsetType] = UNSET,
+    ):
+        """Create a new `MiniJobBase` instance."""
+
+        super().__init__()
+        self._interval_secs = (
+            self.DEFAULT_INTERVAL.total_seconds()
+            if interval is UNSET
+            else interval.total_seconds()
+        )
+        self._count = self.DEFAULT_COUNT if count is UNSET else count
+
+        self._reconnect = not not (
+            self.DEFAULT_RECONNECT if reconnect is UNSET else reconnect
+        )
+
+        self._time = self.DEFAULT_TIME if time is UNSET else time
+
+        self._loop_count = 0
+        self._task_loop = CustomLoop(
+            self._on_run,
+            seconds=self._interval_secs,
+            hours=0,
+            minutes=0,
+            time=self._time or discord.utils.MISSING,
+            count=self._count,
+            reconnect=self._reconnect,
+        )
+
+        self._task_loop.before_loop(self._on_start)
+        self._task_loop.after_loop(self._on_stop)
+        self._task_loop.error(self._on_run_error)
+
+    @property
+    def external_data(self):
+        """The `JobNamespace` instance bound to this job
+        object for storing data to be accessed internally.
+        """
+        return self._external_data
+
+    def next_iteration(self):
+        """When the next iteration of `.on_run()` will occur.
+        If not known, this method will return `None`.
+
+        Returns:
+            Optional[datetime.datetime]: The time at which
+              the next iteration will occur, if available.
+        """
+        return self._task_loop.next_iteration()
+
+    def get_interval(self):
+        """Returns a tuple of the seconds, minutes and hours at which this job
+        object is executing its `.on_run()` method.
+
+        Returns:
+            tuple: `(seconds, minutes, hours)`
+        """
+        return self._task_loop.seconds, self._task_loop.minutes, self._task_loop.hours
+
+    def change_interval(
+        self,
+        *,
+        seconds: float = 0,
+        minutes: float = 0,
+        hours: float = 0,
+        time: Union[datetime.time, Sequence[datetime.time], _UnsetType] = UNSET,
+    ):
+        """Change the interval at which this job will run its `on_run()` method,
+        as soon as possible.
+
+        Args:
+            seconds (float, optional): Defaults to 0.
+            minutes (float, optional): Defaults to 0.
+            hours (float, optional): Defaults to 0.
+            time (Union[datetime.time, Sequence[datetime.time]], optional):
+              Defaults to 0.
+        """
+        self._task_loop.change_interval(
+            seconds=seconds,
+            minutes=minutes,
+            hours=hours,
+            time=time if time is not UNSET else discord.utils.MISSING,
+        )
+
+        if time is not UNSET:
+            self._time = time
+        else:
+            self._interval_secs = seconds + (minutes * 60.0) + (hours * 3600.0)
+
+    async def on_start(self):  # make this optional for subclasses
+        pass
+
+    async def _on_run(self):
+        if self._skip_on_run:
+            return
+
+        self._is_idling = False
+        self._idling_since_ts = None
+
+        await self.on_run()
+        if self._interval_secs:  # There is a task loop interval set
+            self._is_idling = True
+            self._idling_since_ts = time.time()
+
+        self._loop_count += 1
+
+    async def on_run(self):
+        """DO NOT CALL THIS METHOD MANUALLY, EXCEPT WHEN USING `super()` WITHIN
+        OVERLOADED VERSIONS OF THIS METHOD TO ACCESS A SUPERCLASS METHOD.
+
+        The code to run at the set interval.
+        This method must be overloaded in subclasses.
+
+        Raises:
+            NotImplementedError: This method must be overloaded in subclasses.
+        """
+        raise NotImplementedError()
+
+    async def on_stop(self):  # make this optional for subclasses
+        pass
+
+
+async def initialize_minijob(job: MiniJobBase) -> bool:
+    """Initialize the given mini job object.
+
+    Args:
+        job (MiniJobBase): The minijob.
+
+    Returns:
+        bool: Whether the operation was successful.
+
+    Raises:
+        TypeError: The given job instance was not a mini job.
+        JobStateError: The given job has already been initialized.
+    """
+    if not isinstance(job, MiniJobBase):
+        raise TypeError(
+            "argument 'job' must be an instance of "
+            f"MiniJobBase, not {job.__class__.__name__}"
+        )
+    elif job._initialized:
+        raise JobStateError("the given job has already been initialized")
+
+    return await job._INITIALIZE_EXTERNAL()
+
+
+def start_minijob(job: MiniJobBase) -> bool:
+    """Start the given mini job object.
+
+    Args:
+        job (MiniJobBase): The minijob.
+
+    Returns:
+        bool: Whether the operation was successful.
+
+    Raises:
+        TypeError: The given job instance was not a mini job.
+        JobStateError: The given job was not initialized.
+    """
+    if not isinstance(job, MiniJobBase):
+        raise TypeError(
+            "argument 'job' must be an instance of "
+            f"MiniJobBase, not {job.__class__.__name__}"
+        )
+    elif not job._initialized:
+        raise JobStateError("The given mini job was not initialized.")
+
+    return job._START_EXTERNAL()
+
+
+def stop_minijob(job: MiniJobBase, force: bool = False) -> bool:
+    """Stop the given mini job object.
+
+    Args:
+        job (MiniJobBase): The minijob.
+        force (bool): Whether to suspend all operations of the
+          job forcefully.
+
+    Returns:
+        bool: Whether the operation was successful.
+
+    Raises:
+        TypeError: The given job instance was not a mini job.
+    """
+    if not isinstance(job, MiniJobBase):
+        raise TypeError(
+            "argument 'job' must be an instance of "
+            f"MiniJobBase, not {job.__class__.__name__}"
+        )
+    return job._STOP_EXTERNAL(force=force)
+
+
+def restart_minijob(job: MiniJobBase) -> bool:
+    """Restart the given mini job object.
+
+    Args:
+        job (MiniJobBase): The minijob.
+
+    Returns:
+        bool: Whether the operation was successful.
+
+    Raises:
+        TypeError: The given job instance was not a mini job.
+    """
+    if not isinstance(job, MiniJobBase):
+        raise TypeError(
+            "argument 'job' must be an instance of "
+            f"MiniJobBase, not {job.__class__.__name__}"
+        )
+    return job._RESTART_EXTERNAL()
